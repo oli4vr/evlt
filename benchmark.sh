b7decbc1101f8e4042025cf26328e5121ddc19a9
oli4vr <oli4vr@gmail.com>
2024-04-29 11:35:13 +0200

    benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -8,10 +8,18 @@ rm -rf ~/.evlt
 echo
 echo "####Performance"
 echo
+echo "####First data blob write $CNT * $BS"
 dd if=/dev/zero bs=$BS count=$CNT | ./evlt put /myvault/key1/key2/key3 -n $SEG -v
+echo
+echo "####First data blob read $CNT * $BS"
 ./evlt get /myvault/key1/key2/key3 -n $SEG -v | dd of=/dev/null bs=$BS
+echo
+echo "####Second data blob add $CNT * $BS"
 dd if=/dev/zero bs=$BS count=$CNT | ./evlt put /myvault/key1/key2/key4 -n $SEG -v
+echo
+echo "####Second data blob read $CNT * $BS"
 ./evlt get /myvault/key1/key2/key4 -n $SEG -v | dd of=/dev/null bs=$BS
+echo
 rm -rf ~/.evlt 
 
 echo
